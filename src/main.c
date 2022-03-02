b7c6847c44b4ea38067f42ca0cbc7e7a3c9a0dea
Jhonata <50640451+JhonatMiranda@users.noreply.github.com>
2022-03-02 20:45:46 -0300

    menu

--- src/main.c
+++ src/main.c
@@ -4,8 +4,17 @@
 #include "mapa.h"
 #include "memtable.h"
 
-
-int main(void) {
-    leArqv("data/entrada.txt");
+int main(void)
+{
+    char nomeArquivo[100];
+    char data[100];
+    char buffer[100];
+    strcpy(data, "data/");
+    printf("Digite o nome do arquivo desejado: Ex: entrada\n");
+    fgets(buffer, 100, stdin);
+    sscanf(buffer, "%s", nomeArquivo);
+    strcat(nomeArquivo, ".txt");
+    strcat(data, nomeArquivo);
+    leArqv(data);
     return 0;
 }
